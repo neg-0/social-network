7abad48f5c2454d498436b159a719529ec8b3188
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-18 12:43:32 -0400

    Added some news stories and profiles

---
+++ src/components/UserProfileDB.js
@@ -0,0 +1,85 @@
+export const UserProfile =
+{
+    newsArticles: [
+        {
+            title: "Clerk chooses $1 million lottery ticket for Michigan woman",
+            article: `A Michigan woman who allowed a store clerk to pick out her lottery tickets for her ended up winning a $1 million jackpot.
+
+            The 47-year-old Genesee County woman told Michigan Lottery officials she went into the Miller Point Mart in Flint to buy some scratch-off lottery tickets, but she didn't know which tickets she wanted to buy.
+            
+            "I asked the clerk to pick some lottery tickets for me," she said. "I was on my lunch break and decided to scratch them before heading back into work. When I realized I'd won $1 million, I thought for sure I was reading the ticket wrong. I couldn't believe it!"
+            
+            The player won her top prize from a $10 Cash Game ticket.
+            
+            The winner said some of the jackpot will go toward funding a family vacation, and the rest will go into savings.`
+        },
+        {
+            title: `Katie Nicholson Officially 'Old'`,
+            article: `Local resident Katie Nicholson turned 50 today and officially joins the ranks of the wrinkly oldies.
+
+            When asked how she felt, Ms Nicholson said "Oh, it's not so bad turning 50. Sure my joints ache, my hair is greying, and I suffer from hot flashes, but I'm so forgetful these days that I hardly remember to notice the discomfort. In fact, my memory's so bad that I probably won't even remember having this conversation with you. Who are you again, by the way?".
+            
+            Residents have been celebrating Ms Nicholson's birthday, for more reasons than one - her birthday is expected to provide a small boost to the city's economy, as she will soon be looking to purchase new spectacles, arthritis cream, extra-strength wrinkle lotion, and a zimmer frame.`
+        },
+        {
+            title: `Santa Names "World's Naughtiest Boy"`,
+            article: `In a first ever statement of this kind, Santa Clause has stated that he is imposing a lifetime Christmas present embargo on Darren P Smith, after stating that he was "simply appalled" with the behaviour of Darren Smith over the last year.
+
+            One of Santa's inner circle of elves told the Polar Paper: "I've never seen Santa this irate before. He was shouting with rage so much that his cheeks turned purple and his beard was covered in little flecks of spittle. Darren Smith must have done some pretty naughty things to make him this furious. I mean, a lifetime ban on Christmas presents - it's unheard of!".
+            
+            Considerable speculation has circulated regarding precisely what it is that Darren Smith did to arouse Santa's wrath, but no clear answers have emerged.`
+        },
+        {
+            title: `Ohio farmers' 2,164-pound green squash sets world record`,
+            article: `A pair of Ohio farmers took their 2,164-pound green squash to a giant vegetable competition and walked away with a world record.
+
+            Todd and Donna Skinner, who have been growing pumpkins and squash for nearly 30 years, said they knew their green squash was going to be impressive at the Oakland Nursery National Pumpkin Weigh-Off in Dublin, but they were stunned to see it tip the scales at 2,164 pounds.
+            
+            "We had a rough idea that it was going to weigh nice, but not that heavy," Donna Skinner told WTOV-TV.
+            
+            Oakland Nursery said the Skinners are now world record holders for the heaviest green squash.
+            
+            "Truly a sight to be seen," the business said in an Instagram post.`
+        },
+        {
+            title: `Baby bison goes wandering in North Carolina town`,
+            article: `A baby bison caused a stir in North Carolina when it escaped from its owner and was spotted wandering near a school.
+
+            The Brunswick County Sheriff's Office said the 7-month-old calf was spotted wandering in Ash on Thursday and Friday.
+            
+            The animal was seen Friday morning wandering near Jesse Mae Monroe Elementary School.
+            
+            The sheriff's office said the bison was safely captured Friday and returned to its owner.
+            
+            Police in Ohio recently responded to a similar situation when a loose bison was spotted wandering in German Township. Police received multiple reports of sightings from around the township before the animal's owner was able to safely recapture it.`
+        },
+
+        //add more stories here in this array!
+    ],
+    contactProfiles: [
+        {
+            firstName: "Justine",
+            lastName: "Dustice",
+            birthday: "01/01/1978 00:00:00",
+            profileImage: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80"
+        },
+        {
+            firstName: "Willie",
+            lastName: "Dustice",
+            birthday: "01/01/1978 00:00:00",
+            profileImage: "https://images.unsplash.com/photo-1463453091185-61582044d556?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80"
+        },
+        {
+            firstName: "Johannas",
+            lastName: "Dustice",
+            birthday: "01/01/1978 00:00:00",
+            profileImage: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+        },
+        {
+            firstName: "Stu",
+            lastName: "Dustice",
+            birthday: "01/01/1978 00:00:00",
+            profileImage: "https://images.unsplash.com/photo-1506277886164-e25aa3f4ef7f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+        }
+    ]
+}
