c008c393c889882ba059d657d65c3f355b3eb61d
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-18 14:52:28 -0400

    Cleaned up imports

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,8 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
