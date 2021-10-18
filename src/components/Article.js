f9ab3b4405d9d62008b5c7f7698e276dc7cd55a9
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-18 14:33:27 -0400

    Added styling to article

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,8 +2,9 @@ import { Paper, Typography } from "@mui/material";
 
 export default function Article({ article }) {
     return (<article>
-        <Paper elevation={2} sx={{ m: 5, p: 2 }}>
+        <Paper elevation={4} sx={{ m: 5, p: 2 }}>
             <Typography variant="h4">{article.title}</Typography>
+            <hr />
             <Typography variant="p">{article.article}</Typography>
         </Paper>
     </article>)
